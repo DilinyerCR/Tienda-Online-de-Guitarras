5d4bc9eb40d5ce5745c1f533ec9b5a8d267fc686
DilinyerCR <dilinyerecr@gmail.com>
2025-08-16 14:00:52 -0400

    adding changes

--- src/components/Guitar.jsx
+++ src/components/Guitar.jsx
@@ -1,6 +1,6 @@
 
 
-const Guitar = ({guitar}) => {
+const Guitar = ({guitar, addToCart}) => {
 
     const {id, name, price, description, image} = guitar
  
@@ -16,6 +16,7 @@ const Guitar = ({guitar}) => {
                 <button
                     type="button"
                     className="btn btn-dark w-100"
+                    onClick={()=> addToCart(guitar)}
                 >Agregar al Carrito</button>
             </div>
         </div>
